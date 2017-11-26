d234454a40e951e646f45c87a7d4d137e2625d81
Warren Crossing <warren@itcl.com.au>
2017-11-27 03:37:09 +1100

    add local patches

---
+++ patch/node_modules/redux-connect/modules/index.js
@@ -0,0 +1,5 @@
+export ReduxAsyncConnect from './containers/AsyncConnect';
+export { asyncConnect } from './containers/decorator';
+export { loadOnServer } from './helpers/utils';
+export { reducer, immutableReducer } from './store';
+export { setToImmutableStateFunc, setToMutableStateFunc } from './helpers/state';
