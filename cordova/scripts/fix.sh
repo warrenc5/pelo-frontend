5be31ee4d66cb8637dfad45d7a4b6191045503a0
Pelo Dev <pelo-dev@pelo.cc>
2018-01-02 14:25:36 +1100

    fix

--- cordova/scripts/fix.sh
+++ cordova/scripts/fix.sh
@@ -1,6 +1,4 @@
-cp -r `pwd`/platforms/android/res `pwd`/platforms/android/app/src/main/
- ln -sfn `pwd`/platforms/android/app/src/main/res/xml `pwd`/platforms/android/res/xml
- ln -sfn `pwd`/platforms/android/app/src/main/res/values `pwd`/platforms/android/res/values
- ln -sfn `pwd`/platforms/android/app/src/main/AndroidManifest.xml `pwd`/platforms/android/AndroidManifest.xml
- ln -sfn `pwd`/platforms/android/app/src/main/java `pwd`/platforms/android/src
- ln -sfn `pwd`/platforms/android/assets/ `pwd`/platforms/android/app/src/main/assets
+ln -sfn `pwd`/platforms/android/app/src/main/res `pwd`/platforms/android/res
+ln -sfn `pwd`/platforms/android/app/src/main/AndroidManifest.xml `pwd`/platforms/android/AndroidManifest.xml
+ln -sfn `pwd`/platforms/android/app/src/main/java `pwd`/platforms/android/src
+ln -sfn `pwd`/platforms/android/assets/ `pwd`/platforms/android/app/src/main/assets
