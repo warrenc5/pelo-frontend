4dc12fe3534e755bec6c76a0cbd7163f6c568f90
wozza <wozza@pulse.local>
2018-01-04 22:32:20 +1100

    ios build

---
+++ cordova/scripts/ios-appload.sh
@@ -0,0 +1,2 @@
+"/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool" --validate-app -f `find . -name \*.ipa` -u wozza.xing@gmail.com -p @keychain:idmsa.apple.com
+"/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool" --upload-app -f `find . -name \*.ipa` -u wozza.xing@gmail.com -p @keychain:idmsa.apple.com
---
+++ cordova/scripts/ios-archive.sh
@@ -0,0 +1,1 @@
+xcrun xcodebuild -scheme Pelo -configuration Release archive -archivePath platforms/ios/Pelo.xcarchive
