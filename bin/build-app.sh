0d4f3752a602b2977583a74a0b1f9b94e76b450b
wozza <wozza@pulse.local>
2018-01-07 11:21:33 +1100

    build

--- bin/build-app.sh
+++ bin/build-app.sh
@@ -1,11 +1,13 @@
 cd ~/$1
 git checkout $2
 git fetch 
+
 test `git rev-parse HEAD` != `git rev-parse @{u}` 
+
 git checkout -- . 
 git pull --no-commit 
 . ~/.nvm/nvm.sh 
 npm i 
-NODE_ENV=$3 /bin/bash -i -c "gulp release $4"
+NODE_ENV=$3 /bin/bash -i -c "gulp release --platform $4"
 chmod o+r /var/www/html/*
 
