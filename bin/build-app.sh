0123f1b76efcf6a7d8488e1a1bfdb574b96cfb6c
wozza <wozza@pulse.local>
2018-01-07 21:57:36 +1100

    ci/cd

--- bin/build-app.sh
+++ bin/build-app.sh
@@ -2,12 +2,14 @@ cd ~/$1
 git checkout $2
 git fetch 
 
-test `git rev-parse HEAD` != `git rev-parse @{u}` 
+test `git rev-parse HEAD` != `git rev-parse @{u}` || exit 1
 
 git checkout -- . 
 git pull --no-commit 
 . ~/.nvm/nvm.sh 
+rm package-lock.json cordova/package-lock.json
 npm i 
+rm package-lock.json cordova/package-lock.json
 NODE_ENV=$3 /bin/bash -i -c "gulp release --platform $4"
 chmod o+r /var/www/html/*
 
