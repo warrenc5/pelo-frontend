6f89de7bdc4d165076fab8b6e6ec684875982c9d
wozza <wozza@pulse.local>
2018-01-27 18:20:08 +1100

    ci/cd

--- cordova/scripts/ios-archive.sh
+++ cordova/scripts/ios-archive.sh
@@ -1,5 +1,5 @@
-if [[ $CORDOVA_CMD =~ /release/ ]] ; then 
+if [[ $CORDOVA_CMDLINE =~ release ]] ; then 
 xcrun xcodebuild -scheme Pelo -configuration Release archive -archivePath platforms/ios/Pelo.xcarchive
 else
-echo "NOT EXPORTED"
+echo "NOT ARCHIVED **********8"
 fi
